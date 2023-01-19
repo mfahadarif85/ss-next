1036b592bde4188c588c68d02c95dc96663a5db5
mfahadarif85 <mfahadarif@gmail.com>
2023-01-19 20:21:21 +0500

    server side components with async/await to Fetch

--- todo/app/todos/[todoId]/page.tsx
+++ todo/app/todos/[todoId]/page.tsx
@@ -1,3 +1,4 @@
+import { Todo } from "@/typings";
 import React from "react";
 type PageProps = {
   params: {
@@ -5,8 +6,27 @@ type PageProps = {
   };
 };
 
-const TodoPage = ({ params: { todoId } }: PageProps) => {
-  return <div>TodoPage: {todoId}</div>;
+const fetchTodo = async (todoId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  );
+  const todo: Todo = await res.json();
+  return todo;
+};
+
+const TodoPage = async ({ params: { todoId } }: PageProps) => {
+  const todo = await fetchTodo(todoId);
+  return (
+    <div className='p-10 bg-yellow-200 border-2 m-2 shadow-lg'>
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+      <p className='border-t border-black mt-5 text-right'>
+        By User: {todo.userId}
+      </p>
+    </div>
+  );
 };
 
 export default TodoPage;
