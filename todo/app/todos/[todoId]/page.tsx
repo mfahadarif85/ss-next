38145bb095f573b8be19eb6c888fe4f1e2784a9c
mfahadarif85 <mfahadarif@gmail.com>
2023-01-22 16:52:53 +0500

    tailwind pretiier plugin, building out error msg

--- todo/app/todos/[todoId]/page.tsx
+++ todo/app/todos/[todoId]/page.tsx
@@ -1,5 +1,10 @@
 import { Todo } from "@/typings";
 import React from "react";
+
+import { notFound } from "next/navigation";
+
+export const dynamicParams = true;
+
 type PageProps = {
   params: {
     todoId: string;
@@ -8,7 +13,8 @@ type PageProps = {
 
 const fetchTodo = async (todoId: string) => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`,
+    { next: { revalidate: 60 } }
   );
   const todo: Todo = await res.json();
   return todo;
@@ -16,13 +22,18 @@ const fetchTodo = async (todoId: string) => {
 
 const TodoPage = async ({ params: { todoId } }: PageProps) => {
   const todo = await fetchTodo(todoId);
+
+  if (!todo.id) {
+    return notFound();
+  }
+
   return (
-    <div className='p-10 bg-yellow-200 border-2 m-2 shadow-lg'>
+    <div className='m-2 border-2 bg-yellow-200 p-10 shadow-lg'>
       <p>
         #{todo.id}: {todo.title}
       </p>
       <p>Completed: {todo.completed ? "Yes" : "No"}</p>
-      <p className='border-t border-black mt-5 text-right'>
+      <p className='mt-5 border-t border-black text-right'>
         By User: {todo.userId}
       </p>
     </div>
@@ -30,3 +41,14 @@ const TodoPage = async ({ params: { todoId } }: PageProps) => {
 };
 
 export default TodoPage;
+
+export async function generateStaticParams() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const todos: Todo[] = await res.json();
+  // for this DEMO, we are only prebuilding the first 10 pages to avoid being rate limited by the DEMO API
+  const trimmedTodos = todos.splice(0, 10);
+
+  return trimmedTodos.map((todo) => ({
+    todoId: todo.id.toString(),
+  }));
+}
